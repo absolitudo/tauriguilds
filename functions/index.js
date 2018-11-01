e420403771727970b880e746798b63a77d1409a8
Ákos <absolitudo@gmail.com>
2018-11-01 13:10:41 +0100

    setup database

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,23 @@
+const firebase = require("firebase-admin");
 const functions = require("firebase-functions");
+firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+    firebase
+        .database()
+        .ref("tauriguilds")
+        .set({
+            hi: "hello"
+        })
+        .then(something => response.send("set?"));
+});
+
+exports.getGuilds = functions.https.onRequest((request, response) => {
+    firebase
+        .database()
+        .ref("tauriguilds")
+        .once("value")
+        .then(snapshot => {
+            response.send(snapshot.val());
+        });
 });
