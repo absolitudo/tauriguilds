3ebfe2535b4e459406f7576bb339d814b3b2b4c1
Ákos <absolitudo@gmail.com>
2018-11-27 22:28:17 +0100

    refactor, include raid progress abbrevations in guild data

---
+++ helpers.js
@@ -0,0 +1,186 @@
+require("dotenv").config();
+const raidsConst = require("./raids.json");
+const abbreviations = require("./abbreviations.json");
+const TauriApi = require("./tauriApi");
+const tauri = new TauriApi(
+    process.env.TAURI_API_KEY,
+    process.env.TAURI_API_SECRET
+);
+
+async function getGuildData(guildName, realm) {
+    return new Promise(async (resolve, reject) => {
+        let guildData = await tauri.getGuild(guildName, realm);
+
+        if (!guildData.success) {
+            reject(guildData.errorstring);
+        } else {
+            const trimmedRoster = trimRoster(guildData.response.guildList);
+            const rosterAchievements = await getRosterAchievements(
+                trimmedRoster
+            );
+            const progression = getProgression(rosterAchievements);
+
+            guildData.response.progression = progression;
+            guildData.response.lastUpdated = new Date().getTime() / 1000;
+
+            resolve({
+                compact: getCompactGuildData(guildData.response),
+                extended: guildData.response
+            });
+        }
+    });
+}
+
+function trimRoster(roster) {
+    let trimmedRoster = [];
+    let memberMuliplyr = 0;
+
+    for (let member in roster) {
+        memberMuliplyr++;
+        if (roster[member].rank < 5) {
+            trimmedRoster.push(roster[member]);
+        }
+    }
+
+    memberMuliplyr = memberMuliplyr > 80 ? memberMuliplyr / 80 : 1;
+    let memberCounter = 0;
+
+    trimmedRoster = trimmedRoster.filter(member => {
+        let rando = Math.round(Math.random()) === 1;
+        if (
+            memberCounter < 10 ||
+            (rando && memberCounter < 20 * memberMuliplyr) ||
+            member.rank === 0
+        ) {
+            memberCounter++;
+            return true;
+        }
+        return false;
+    });
+
+    return trimmedRoster;
+}
+
+async function getRosterAchievements(roster) {
+    let rosterAchievements = [];
+
+    for (let member of roster) {
+        rosterAchievements.push(await tauri.getAchievements(member.name));
+    }
+
+    return rosterAchievements
+        .filter(data => data.success)
+        .map(data => data.response);
+}
+
+function getProgression(roster) {
+    let raids = { ...raidsConst };
+
+    for (let member of roster) {
+        for (let achievement in member["Achievements"]) {
+            const achievementName = member["Achievements"][achievement].name;
+            if (raids["Throne of Thunder"][achievementName]) {
+                if (
+                    typeof raids["Throne of Thunder"][achievementName] ===
+                    "boolean"
+                ) {
+                    raids["Throne of Thunder"][achievementName] = 0;
+                }
+                raids["Throne of Thunder"][achievementName] += 1;
+            } else if (
+                raids["Terrace of the Endless Spring"][achievementName]
+            ) {
+                if (
+                    typeof raids["Terrace of the Endless Spring"][
+                        achievementName
+                    ] === "boolean"
+                ) {
+                    raids["Terrace of the Endless Spring"][achievementName] = 0;
+                }
+                raids["Terrace of the Endless Spring"][achievementName] += 1;
+            } else if (raids["Heart of Fear"][achievementName]) {
+                if (
+                    typeof raids["Heart of Fear"][achievementName] === "boolean"
+                ) {
+                    raids["Heart of Fear"][achievementName] = 0;
+                }
+                raids["Heart of Fear"][achievementName] += 1;
+            } else if (raids["Mogu'shan Vaults"][achievementName]) {
+                if (
+                    typeof raids["Mogu'shan Vaults"][achievementName] ===
+                    "boolean"
+                ) {
+                    raids["Mogu'shan Vaults"][achievementName] = 0;
+                }
+                raids["Mogu'shan Vaults"][achievementName] += 1;
+            }
+        }
+    }
+
+    for (let instance in raids) {
+        for (let achievement in raids[instance]) {
+            if (raids[instance][achievement] > 3) {
+                raids[instance][achievement] = true;
+            } else {
+                raids[instance][achievement] = false;
+            }
+        }
+    }
+
+    raids = abbreviateProgression(raids);
+
+    return raids;
+}
+
+function getCompactGuildData(guildData) {
+    const {
+        progression,
+        guildName,
+        lastUpdated,
+        gFaction,
+        realm,
+        guildMembersCount
+    } = guildData;
+    return {
+        progression,
+        guildName,
+        gFaction,
+        realm,
+        lastUpdated,
+        guildMembersCount
+    };
+}
+
+function whenWas(time) {
+    return Math.round((new Date().getTime() / 1000 - Number(time)) / 3600);
+}
+
+function abbreviateProgression(progression) {
+    for (let raid in raidsConst) {
+        let totalBosses = 0;
+        let heroicDefeated = 0;
+
+        for (let boss in progression[raid]) {
+            if (progression[raid][boss]) {
+                heroicDefeated++;
+            }
+            totalBosses++;
+        }
+
+        progression[raid].abbreviation = `${
+            abbreviations[raid]
+        } ${heroicDefeated}/${totalBosses} HC`;
+    }
+
+    return progression;
+}
+
+module.exports = {
+    getGuildData,
+    trimRoster,
+    getRosterAchievements,
+    getProgression,
+    getCompactGuildData,
+    whenWas,
+    abbreviateProgression
+};
