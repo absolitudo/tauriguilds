df43aaa0a9b3c6bbeff25b4ff3f870e28b187bd5
Ákos <absolitudo@gmail.com>
2018-12-07 09:31:51 +0100

    validate player request

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,4 @@
-function valiDateGuildRequest(req, res, next) {
-    const guildName = req.body.guildName;
-
+function validateGuildRequest(req, res, next) {
     try {
         req.body.guildName = req.body.guildName.trim().replace(/\s+/, " ");
     } catch (err) {
@@ -15,6 +13,29 @@ function valiDateGuildRequest(req, res, next) {
     next();
 }
 
+function validatePlayerRequest(req, res, next) {
+    try {
+        req.body.guildName = req.body.guildName.trim().replace(/\s+/, " ");
+    } catch (err) {
+        res.send(JSON.stringify({ err: "Invalid guild name." }));
+    }
+
+    try {
+        req.body.playerName = req.body.playerName.trim().replace(/\s+/, " ");
+    } catch (err) {
+        res.send(JSON.stringify({ err: "Invalid player name." }));
+    }
+
+    try {
+        req.body.realm = req.body.realm.trim().replace(/\s+/, " ");
+    } catch (err) {
+        res.send(JSON.stringify({ err: "Invalid realm name." }));
+    }
+
+    next();
+}
+
 module.exports = {
-    valiDateGuildRequest
+    validateGuildRequest,
+    validatePlayerRequest
 };
